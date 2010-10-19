d1980827b90654fc01649a187681487045de9e26
Alejandro Martinez Ruiz <alex@flawedcode.org>
2010-10-19 13:38:08 +0200

    do not fail spec task, mark as pending

--- spec/ookie_spec.rb
+++ spec/ookie_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Ookie" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "is pending" do
+    pending "proper testing is underway"
   end
 end
