07cbf7a79eb8d0928949be96118daa78ae1e11d6
Alejandro Martinez Ruiz <alex@flawedcode.org>
2010-10-23 16:49:47 +0200

    prepare for 0.3.0 release

--- lib/ookie/version.rb
+++ lib/ookie/version.rb
@@ -11,8 +11,8 @@
 module Ookie
   module Version
     MAJOR = 0
-    MINOR = 2
-    PATCH = 1
+    MINOR = 3
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
